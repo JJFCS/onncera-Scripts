956228a2e75f7e96ed9ca717a1f2d4df8ad13cb0
onncera-shogun <onncera@localhost.com>
2024-05-10 11:10:54 +0800

    Formatting

--- eos i3 setup/000-performance.sh
+++ eos i3 setup/000-performance.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-
+#
+#
 # Check for updates on the script (https://gitlab.com/cscs/maxperfwiz)
 # Credit goes to the original author in the Gitlab link above
 
-
-cd ~/Downloads/
 git clone https://gitlab.com/cscs/maxperfwiz
 git clone https://github.com/arcolinux/arcolinux-spices
-cd ~/Downloads/maxperfwiz/
 
+mv maxperfwiz/ ~/Downloads/maxperfwiz
+mv arcolinux/ ~/Downloads/arcolinux
+
+cd ~/Downloads/maxperfwiz
 chmod +x maxperfwiz
-
